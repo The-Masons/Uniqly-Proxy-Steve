c988fc661e77298791ac1b122218bbc8691c70ea
StevenPurn <smp5119@gmail.com>
2018-04-27 21:34:25 -0700

    refactor  db calls

--- server/server.js
+++ server/server.js
@@ -18,36 +18,58 @@ app.get('/product/:productid', (req, res) => {
 
 app.get('/product/:productId/sizes_qtys', (req, res) => {
   axios.get(`http://13.57.205.67:3001/product/${req.params.productId}/sizes_qtys`)
-    .then(resp => res.status(200).send(resp.data))
-    .catch(err => res.status(500).send(err));
+    .then((data) => {
+      res.status(200).send(data.data)
+    })
+    .catch((err) => {
+      res.status(500).send(err)
+    });
 });
 
 app.get('/product/:productId/addtocart', (req, res) => {
   axios.get(`http://13.57.205.67:3001/product/${req.params.productId}/addtocart`)
-    .then(resp => res.status(200).send(resp.data))
-    .catch(err => res.status(500).send(err));
+    .then((data) => {
+      res.status(200).send(data.data)
+    })
+    .catch((err) => {
+      res.status(500).send(err)
+    });
 });
 
 app.get('/product/:productId/colors', (req, res) => {
   axios.get(`http://54.183.208.237:3003/product/${req.params.productId}/colors`)
-    .then(resp => res.status(200).send(resp.data))
-    .catch(err => res.status(500).send(err));
+    .then((data) => {
+      res.status(200).send(data.data)
+    })
+    .catch((err) => {
+      res.status(500).send(err)
+    });
 });
 
 app.get('/product/:productId/images', (req, res) => {
   axios.get(`http://54.183.208.237:3003/product/${req.params.productId}/images`)
-    .then(resp => res.status(200).send(resp.data))
-    .catch(err => res.status(500).send(err));
+    .then((data) => {
+      res.status(200).send(data.data)
+    })
+    .catch((err) => {
+      res.status(500).send(err)
+    });
 });
 
 app.get('/productdetails/:productId', (req, res) => {
   axios.get(`http://54.183.208.237:3003/productdetails/${req.params.productId}`)
-    .then(resp => res.status(200).send(resp.data))
-    .catch(err => res.status(500).send(err));
+    .then(data => res.status(200).send(data.data))
+    .catch((err) => {
+      res.status(500).send(err)
+    });
 });
 
 app.get('/reviews/:productId', (req, res) => {
   axios.get(`http://13.58.109.199:3005/reviews/${req.params.productId}`)
-    .then(resp => res.status(200).send(resp.data))
-    .catch(err => res.status(500).send(err));
+    .then((data) => {
+      res.status(200).send(data.data)
+    })
+    .catch((err) => {
+      res.status(500).send(err)
+    });
 });
